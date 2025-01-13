b7369f89ea70d87b5ccd209ff580bfb00ecda1b4
simotasca <simo.tasca@gmail.com>
2025-01-13 19:56:49 +0100

    design improvements

--- src/global.ts
+++ src/global.ts
@@ -1,2 +1,4 @@
+const dateFormatter = Intl.DateTimeFormat("it-IT", { dateStyle: "medium" });
+
 String.prototype.capitalize = function () { return this[0].toUpperCase() + this.slice(1) }
-Date.prototype.format = function () { return Intl.DateTimeFormat("it-IT").format(this) }
+Date.prototype.format = function () { return dateFormatter.format(this) }
