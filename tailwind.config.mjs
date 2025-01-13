c0a9091d9ef068900997ce44fee80edb5dd217ac
simotasca <simo.tasca@gmail.com>
2025-01-13 19:55:57 +0100

    font management

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
 import plugin from "tailwindcss/plugin";
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"UbuntuSans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     plugin(({ addBase, theme }) => {
